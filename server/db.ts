333af80ca022287919b200c00380b8da9c197e9a
tiesen243 <ttien56906@gmail.com>
2025-02-26 21:35:15 +0700

    feat(auth): create session class

--- server/db.ts
+++ server/db.ts
@@ -1,7 +1,15 @@
 import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+import { env } from '@/env'
 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+const createPrismaClient = () =>
+  new PrismaClient({
+    log: env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+  })
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+const globalForPrisma = globalThis as unknown as {
+  prisma: ReturnType<typeof createPrismaClient> | undefined
+}
+
+export const db = globalForPrisma.prisma ?? createPrismaClient()
+if (env.NODE_ENV !== 'production') globalForPrisma.prisma = db
